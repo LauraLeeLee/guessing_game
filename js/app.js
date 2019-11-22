9779b28adf977115b4e9be34917461ba4a6a8bb4
Laura Logan <laurardh@aol.com>
2019-11-22 16:50:58 -0500

    restructures html to hard code and hide divs until triggered

--- js/app.js
+++ js/app.js
@@ -1,38 +1,75 @@
+let chooseGameDiv = document.getElementById('choose-game');
+let guessGameBtn = document.getElementById('guess-btn');
+let reverseGameBtn = document.getElementById('reverse-btn');
+let exitBtn = document.getElementById('exit-btn');
+
+let helloName = document.getElementById('hello-name');
+
+let normalGameDiv = document.getElementById('normal-game');
+
+let minRangeInput = document.getElementById('min-input');
+let minValue = minRangeInput.value;
+
+let maxRangeInput = document.getElementById('max-input');
+let maxValue = maxRangeInput.value;
+
+let userGuess = document.getElementById('user-guess');
+let guessValue = userGuess.value;
+
+let guessResponseDiv = document.createElement('response-div');
+let guessResponse = document.getElementById('guess-response');
+
 
  document.getElementById('submit-input').addEventListener('click', function() {
-    let userName = document.getElementById('name-input').value;
-    console.table(userName);
-
+	let userName = document.getElementById('name-input').value;
     if(userName) {
-       let helloName = document.createElement('H2');
-       helloName.innerHTML = 'Welcome to Guessing Game ' + userName;
-       helloName.classList.add('hello-name');
-       helloName.setAttribute('id', 'hello-name');
-       document.body.appendChild(helloName);
-
+			helloName.innerHTML += userName;
+			chooseGameDiv.style.display = 'block';
       document.getElementById('input-div').classList.toggle('hide-inputs');
-
-      let chooseGameDiv = document.createElement('DIV');
-          chooseGameDiv.classList.add('choose-game');
-          chooseGameDiv.setAttribute('id', 'choose-game');
-      document.body.appendChild(chooseGameDiv);
-
-      let guessGameBtn = document.createElement('button');
-          guessGameBtn.classList.add('guess-btn', 'button');
-          guessGameBtn.setAttribute('id', 'guess-btn');
-          guessGameBtn.innerHTML = 'Play Guessing Game';
-      chooseGameDiv.appendChild(guessGameBtn);
-
-      let reverseGameBtn = document.createElement('button');
-          reverseGameBtn.classList.add('reverse-btn', 'button');
-          reverseGameBtn.setAttribute('id', 'reverse-btn');
-          reverseGameBtn.innerHTML = 'Play Reverse Guessing Game';
-     chooseGameDiv.appendChild(reverseGameBtn);
-
-      let exitBtn = document.createElement('button');
-          exitBtn.classList.add('exit-btn', 'button' );
-          exitBtn.setAttribute('id', 'exit-btn');
-          exitBtn.innerHTML = 'Exit from all games to play';
-     chooseGameDiv.appendChild(exitBtn);
+    } else {
+        alert('Please enter name');
     }
+
+    if(exitBtn) {
+			exitBtn.addEventListener('click', function() {
+				location.reload();
+			});
+		}
+		
+		if(guessGameBtn) {   
+			guessGameBtn.addEventListener('click', function() {
+				console.log('guess game button clicked');
+				normalGameDiv.style.display = 'block';
+				chooseGameDiv.style.display = 'none';
+
+				let minValue = minRangeInput.addEventListener('input', function() {
+					minRangeInput.value;
+					console.log(minRangeInput.value);
+				});
+				
+				let maxValue = maxRangeInput.addEventListener('input', function() {
+					maxRangeInput.value;
+					console.log(maxRangeInput.value);
+				});
+
+				let guessValue = userGuess.addEventListener('input', function() {
+					 userGuess.value;
+					console.log(userGuess.value);
+				});
+
+				if(guessValue && minValue) {
+					if(guessValue < minValue) {
+						guessResponse.innerHTML = "please make guess between " + 
+										minValue + " and " + maxValue;
+										console.log('guess response set');
+					} else {
+						guessResponse.innerHTML = "where are the numbers?";
+						console.log('guess response sort of set');
+					}
+				}
+					
+			});
+    }       
+    
 });
+ 
