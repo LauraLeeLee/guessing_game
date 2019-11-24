10fba39b5563c5f4641078bc617555643d47fef2
Laura Logan <laurardh@aol.com>
2019-11-23 22:23:18 -0500

    adds function for random number edits display for elements

--- js/app.js
+++ js/app.js
@@ -19,6 +19,27 @@ let guessValue;
 let guessResponseDiv = document.createElement('response-div');
 let guessResponse = document.getElementById('guess-response');
 
+let randomNum;
+
+let numbersGuessed = [];
+
+function generateRandomNum(min, max) {
+	randomNum = Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function getMinRangeInput(){
+	minRangeInput.addEventListener('input', function() {
+		console.log(minRangeInput.value);	 
+		minValue = parseInt(minRangeInput.value);	
+	});	
+} 
+
+function getMaxRangeInput(callback) {
+	maxRangeInput.addEventListener('input', function() {
+		maxValue = parseInt(maxRangeInput.value);
+		console.log(maxRangeInput.value);
+	});
+}
 
  document.getElementById('submit-input').addEventListener('click', function() {
 	let userName = document.getElementById('name-input').value;
@@ -41,37 +62,58 @@ let guessResponse = document.getElementById('guess-response');
 				console.log('guess game button clicked');
 				normalGameDiv.style.display = 'block';
 				chooseGameDiv.style.display = 'none';
-
-				let minValue = minRangeInput.addEventListener('input', function() {
-					minRangeInput.value;
-					console.log(minRangeInput.value);
-				});
-		
-				let maxValue = maxRangeInput.addEventListener('input', function() {
-					maxRangeInput.value;
-					console.log(maxRangeInput.value);
-				});
-		
-				let guessValue = userGuess.addEventListener('input', function() {
-					 userGuess.value;
-					console.log(userGuess.value);
-				});
-
 		
 
-				if(guessValue && minValue) {
-					console.log("guessValue minVlaue present?");
-					if(guessValue < minValue) {
-						guessResponse.innerHTML = "please make guess between " + 
-										minValue + " and " + maxValue;
-										console.log('guess response set');
-					} else {
-						guessResponse.innerHTML = "where are the numbers?";
-						console.log('guess response sort of set');
+				getMinRangeInput();
+				getMaxRangeInput();
+				// generateRandomNum(minValue, maxValue);
+				// 	console.log('random: ', randomNum);
+					//random num is generated before 
+					//maxValue is entered
+				
+				document.getElementById('gen-random').addEventListener('click', function(){
+					document.getElementById('min-max-div').style.display = 'none';
+					document.getElementById('input-div23').style.display = 'block';
+					document.getElementById('gen-random').style.display = 'none';
+					generateRandomNum(minValue, maxValue);
+					console.log('random: ', randomNum);
+
+				}); //random generated here doesn't allow for
+				//comparing in if statement below.
+			
+				userGuess.addEventListener('input', function() {			 
+					guessValue = parseInt(userGuess.value);
+					console.log(typeof(minValue), minValue, typeof(maxValue), maxValue, typeof(guessValue), guessValue);
+				// 	generateRandomNum(minValue, maxValue);
+				// console.log('random: ', randomNum);
+				//generateRandomNum here causes new random number
+				//to generate on each guess
+
+					if(guessValue > randomNum) {
+						guessResponse.innerHTML = 'Guess lower';
+						numbersGuessed.push(guessValue);
 					}
-				}
+					if(guessValue < randomNum) {
+						guessResponse.innerHTML = 'Guess higher';
+						numbersGuessed.push(guessValue);
 					
-			});
+					}
+					if(guessValue === randomNum) {
+						numbersGuessed.push(guessValue);
+						guessResponse.innerHTML = 'YOU GUESSED IT!  The number was ' 
+													+ randomNum + ' You guess it in ' 
+													+ numbersGuessed.length  + ' guesses.  The numbers you guessed are '
+													+ numbersGuessed;
+					}
+
+					if(guessValue && minValue && maxValue) {
+						if((guessValue < minValue) || (guessValue > maxValue)) {
+							guessResponse.innerHTML = "please make guess beteween " + 
+											minValue + " and " + maxValue;
+						} 
+					}
+				}); //end of userGuess event
+			}); //end of normalGameBtn event
     }       
     
 });
