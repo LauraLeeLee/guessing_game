e88a2cb818ca07a29cb6867fb762e044ae48f68e
Laura Logan <laurardh@aol.com>
2019-11-25 14:13:28 -0500

    adds playGameAgain function

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ let chooseGameDiv = document.getElementById('choose-game');
 let normalGameBtn = document.getElementById('normal-guess-btn');
 let reverseGameBtn = document.getElementById('reverse-btn');
 let exitBtn = document.getElementById('exit-btn');
-
+let playAgain = document.getElementById('play-again');
 let helloName = document.getElementById('hello-name');
 
 let normalGameDiv = document.getElementById('normal-game');
@@ -16,7 +16,7 @@ let maxValue;
 let userGuess = document.getElementById('user-guess');
 let guessValue;
 
-let guessResponseDiv = document.createElement('response-div');
+let guessResponseDiv = document.getElementById('response-div');
 let guessResponse = document.getElementById('guess-response');
 
 let randomNum;
@@ -34,12 +34,105 @@ function getMinRangeInput(){
 	});	
 } 
 
-function getMaxRangeInput(callback) {
+function getMaxRangeInput() {
 	maxRangeInput.addEventListener('input', function() {
 		maxValue = parseInt(maxRangeInput.value);
 		console.log(maxRangeInput.value);
 	});
 }
+
+function minMaxRange(min, max) {
+	if(min && max) {
+		if((max - min) < 10) {
+			console.log('max- min <10');
+			alert("Min and Max need to have a range of at least 10");
+		}
+	}
+}
+
+function playGameAgain() {
+	document.getElementById('min-max-div').style.display = 'block';
+		document.getElementById('input-div3').style.display = 'none';
+		document.getElementById('gen-random').style.display = 'inline-block';
+		minRangeInput.value = '';
+		maxRangeInput.value = '';
+		userGuess.value = '';
+		guessResponse.innerHTML = '';
+		numbersGuessed = [];
+		randomNum = '';
+		guessResponseDiv.style.display = 'none';
+		playAgain.style.display = 'none';
+		exitBtn.style.display = 'none';
+}
+
+function normalGame() {
+	console.log('guess game button clicked');
+		normalGameDiv.style.display = 'block';
+		chooseGameDiv.style.display = 'none';
+
+		getMinRangeInput();
+		getMaxRangeInput();
+		// minMaxRange(minValue, maxValue);
+
+		document.getElementById('gen-random').addEventListener('click', function(){
+			document.getElementById('min-max-div').style.display = 'none';
+			document.getElementById('input-div3').style.display = 'block';
+			document.getElementById('gen-random').style.display = 'none';
+			generateRandomNum(minValue, maxValue);
+			console.log('random: ', randomNum);
+		}); 
+	
+		userGuess.addEventListener('input', function() {	
+			guessResponseDiv.style.display = 'block';		 
+			guessValue = parseInt(userGuess.value);
+			console.log(typeof(minValue), minValue, typeof(maxValue), maxValue, typeof(guessValue), guessValue);
+
+			if(guessValue > randomNum) {
+				guessResponse.innerHTML = 'Guess lower';
+				numbersGuessed.push(guessValue);
+			}
+			if(guessValue < randomNum) {
+				guessResponse.innerHTML = 'Guess higher';
+				numbersGuessed.push(guessValue);
+			
+			}
+			if(guessValue === randomNum) {
+				numbersGuessed.push(guessValue);
+				guessResponseDiv.style.display = 'block';
+				guessResponse.innerHTML = 'YOU GUESSED IT!  The number was ' 
+											+ randomNum + ' You guess it in ' 
+											+ numbersGuessed.length  + ' guesses.  The numbers you guessed are '
+											+ numbersGuessed;
+				playAgain.style.display = 'inline-block';
+
+						//TODO: make this know which game was played when playAgain
+		//was chosen to start the appropriate game
+			if(playAgain) {
+				playAgain.addEventListener('click', playGameAgain());
+				// 	document.getElementById('min-max-div').style.display = 'block';
+				// 	document.getElementById('input-div3').style.display = 'none';
+				// 	document.getElementById('gen-random').style.display = 'inline-block';
+				// 	minRangeInput.value = '';
+				// 	maxRangeInput.value = '';
+				// 	userGuess.value = '';
+				// 	guessResponse.innerHTML = '';
+				// 	numbersGuessed = [];
+				// 	randomNum = '';
+				// 	guessResponseDiv.style.display = 'none';
+				// 	playAgain.style.display = 'none';
+				// 	exitBtn.style.display = 'none';
+				// });
+			}
+		}
+
+			if(guessValue && minValue && maxValue) {
+				if((guessValue < minValue) || (guessValue > maxValue)) {
+					guessResponse.innerHTML = "please make guess beteween " + 
+									minValue + " and " + maxValue;
+				} 
+			}
+		}); //end of userGuess event
+}//end of normalGame
 
  document.getElementById('submit-input').addEventListener('click', function() {
 	let userName = document.getElementById('name-input').value;
@@ -48,7 +141,8 @@ function getMaxRangeInput(callback) {
 			chooseGameDiv.style.display = 'block';
       document.getElementById('input-div').classList.toggle('hide-inputs');
     } else {
-        alert('Please enter name');
+				alert('Please enter name');
+				location.reload();//TODO: why is this needed now? 
     }
 
     if(exitBtn) {
@@ -56,64 +150,9 @@ function getMaxRangeInput(callback) {
 				location.reload();
 			});
 		}
-		
+
 		if(normalGameBtn) {   
-			normalGameBtn.addEventListener('click', function() {
-				console.log('guess game button clicked');
-				normalGameDiv.style.display = 'block';
-				chooseGameDiv.style.display = 'none';
-		
-
-				getMinRangeInput();
-				getMaxRangeInput();
-				// generateRandomNum(minValue, maxValue);
-				// 	console.log('random: ', randomNum);
-					//random num is generated before 
-					//maxValue is entered
-				
-				document.getElementById('gen-random').addEventListener('click', function(){
-					document.getElementById('min-max-div').style.display = 'none';
-					document.getElementById('input-div23').style.display = 'block';
-					document.getElementById('gen-random').style.display = 'none';
-					generateRandomNum(minValue, maxValue);
-					console.log('random: ', randomNum);
-
-				}); //random generated here doesn't allow for
-				//comparing in if statement below.
-			
-				userGuess.addEventListener('input', function() {			 
-					guessValue = parseInt(userGuess.value);
-					console.log(typeof(minValue), minValue, typeof(maxValue), maxValue, typeof(guessValue), guessValue);
-				// 	generateRandomNum(minValue, maxValue);
-				// console.log('random: ', randomNum);
-				//generateRandomNum here causes new random number
-				//to generate on each guess
-
-					if(guessValue > randomNum) {
-						guessResponse.innerHTML = 'Guess lower';
-						numbersGuessed.push(guessValue);
-					}
-					if(guessValue < randomNum) {
-						guessResponse.innerHTML = 'Guess higher';
-						numbersGuessed.push(guessValue);
-					
-					}
-					if(guessValue === randomNum) {
-						numbersGuessed.push(guessValue);
-						guessResponse.innerHTML = 'YOU GUESSED IT!  The number was ' 
-													+ randomNum + ' You guess it in ' 
-													+ numbersGuessed.length  + ' guesses.  The numbers you guessed are '
-													+ numbersGuessed;
-					}
-
-					if(guessValue && minValue && maxValue) {
-						if((guessValue < minValue) || (guessValue > maxValue)) {
-							guessResponse.innerHTML = "please make guess beteween " + 
-											minValue + " and " + maxValue;
-						} 
-					}
-				}); //end of userGuess event
-			}); //end of normalGameBtn event
+			normalGameBtn.addEventListener('click', normalGame());
     }       
     
 });
